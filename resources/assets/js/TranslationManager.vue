16c1cae8b5957f9fa63b907c5c0c82508cd0c58e
FabianHippmann <fabian.hippmann@moonshiner.at>
2018-01-02 09:43:16 +0100

    add mising key for translationmanager

--- resources/assets/js/TranslationManager.vue
+++ resources/assets/js/TranslationManager.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
-    	<div class="row">
-		    <div class="medium-12 columns">
-		      <label>
-		        <input type="text" v-model="searchTerm" placeholder="Suche">
-		      </label>
-		    </div>
-		</div>
-		<div class="row">
-			 <div class="medium-6 columns">
+        <div class="row">
+            <div class="medium-12 columns">
+              <label>
+                <input type="text" v-model="searchTerm" placeholder="Suche">
+              </label>
+            </div>
+        </div>
+        <div class="row">
+             <div class="medium-6 columns">
           <a class="button" v-on:click="searchTranslations">Search</a>
-		     	<a class="button" v-on:click="clearCache">Clear Cache</a>
+                <a class="button" v-on:click="clearCache">Clear Cache</a>
 
-		    </div>
-		</div>
-    	<snippet :prefix="prefix" v-for="snippet in snippets" :item="snippet"></snippet>
+            </div>
+        </div>
+        <snippet :key="key(snippet)" :prefix="prefix" v-for="snippet in snippets" :item="snippet"></snippet>
     </div>
 </template>
 <script>
@@ -26,20 +26,23 @@ export default {
     }
   },
    mounted(){
-   		console.log('mounted');
-   		this.searchTranslations(); 
+        console.log('mounted');
+        this.searchTranslations();
    },
    methods: {
-   		searchTranslations(){
-   			axios.get(this.prefix+'/search', {
-			    params: {
-			      s: this.searchTerm
-			    }
-			  }).then((response) => {
-			  	this.snippets = response.data;
+        key(snippet){
+            return `${snippet.key}-${snippet.key}`;
+        },
+        searchTranslations(){
+            axios.get(this.prefix+'/search', {
+                params: {
+                  s: this.searchTerm
+                }
+              }).then((response) => {
+                this.snippets = response.data;
 
-			  });
-   		},
+              });
+        },
       clearCache(){
         axios.post(`${this.prefix}/clearCache`).then((response) => {
           console.log('clear');
@@ -48,15 +51,15 @@ export default {
    },
    components:{
       'snippet': require('./Snippet.vue')
-   		
+
    },
    data(){
-   		return {
-   			'snippets': [],
-   			'searchTerm': ''
-   		};
+        return {
+            'snippets': [],
+            'searchTerm': ''
+        };
    }
-    
+
 }
 </script>
 vc
